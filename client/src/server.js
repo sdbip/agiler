e6b926bac20f0f2907852d9f457ebd3268f271d4
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-09-16 20:56:24 +0200

    Require port number to be specified

--- client/src/server.js
+++ client/src/server.js
@@ -2,13 +2,15 @@ const http = require('http')
 let server
 module.exports = {
   listenAtPort: (port) => {
+    if (!port) throw new Error('called without port number')
+
     server = http.createServer()
     server.on('request', (request, response) => {
       response.end('foo')
     })
     server.listen(port)
   },
-  stopListening: () => {
-    server?.close()
+  stopListening: (callback) => {
+    server?.close(callback)
   },
 }
--- client/test/server.spec.js
+++ client/test/server.spec.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const expect = require('chai').expect
+const assert = require('chai').assert
 const http = require('http')
 const server = require('../src/server')
 
@@ -23,6 +24,18 @@ describe('server', () => {
       })
       response.on('end', done) // TODO: This triggers extra error if the test fails
     })
+  })
+
+  it('yields an error if stopping twice', (done) => {
+    server.stopListening()
+    server.stopListening((error) => {
+      if (!error) assert.fail()
+      done()
+    })
+  })
+
+  it('throws if port number is missing', () => {
+    expect(() => server.listenAtPort()).to.throw()
   })
 
   it('can start and stop multiple times', (done) => {
