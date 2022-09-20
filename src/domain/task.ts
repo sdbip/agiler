18524aad808e6f4a002f78d865bc1707b3def816
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-09-20 22:06:52 +0200

    Use the keys of Progress instead of uninformative numbers

--- src/domain/task.ts
+++ src/domain/task.ts
@@ -38,7 +38,7 @@ export interface TaskState {
 }
 
 export enum Progress {
-  notStarted = 0,
-  inProgress = 1,
-  completed = 2
+  notStarted,
+  inProgress,
+  completed
 }
--- src/pg-task-repository.ts
+++ src/pg-task-repository.ts
@@ -1,6 +1,6 @@
 import pg from 'pg'
 import { TaskRepository } from './backend'
-import { Task, TaskState } from './domain/task'
+import { Progress, Task, TaskState } from './domain/task'
 
 class PGTaskRepository implements TaskRepository {
   private client: pg.Client
@@ -10,7 +10,7 @@ class PGTaskRepository implements TaskRepository {
   }
 
   async getNew() {
-    const res = await this.client.query('SELECT * FROM Tasks WHERE progress = 0')
+    const res = await this.client.query('SELECT * FROM Tasks WHERE progress = $1', [ Progress[Progress.notStarted] ])
     return res.rows.map(task)
   }
 
@@ -20,7 +20,7 @@ class PGTaskRepository implements TaskRepository {
   }
 
   async add(task: Task) {
-    await this.client.query('INSERT INTO Tasks (id, title, progress) VALUES ($1, $2, $3)', [ task.id, task.title, task.progress ])
+    await this.client.query('INSERT INTO Tasks (id, title, progress) VALUES ($1, $2, $3)', [ task.id, task.title, Progress[task.progress] ])
   }
 
   async close() {
@@ -38,13 +38,13 @@ export async function connect(database: string) {
 type TaskRow = {
   id: string
   title: string
-  progress: number
+  progress: keyof typeof Progress
 }
 
 function task(row: TaskRow) {
   const props: TaskState = {
     title: row.title,
-    progress: row.progress,
+    progress: Progress[row.progress],
   }
   return Task.reconstitute(row.id, props)
 }
