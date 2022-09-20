ac124c7ead7afdb92c131f740738ef9730fff3a8
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-09-20 06:30:31 +0200

    Move database name to an untracked .env file

--- run.ts
+++ run.ts
@@ -1,8 +1,15 @@
 import { connect } from './src/pg-task-repository.js'
 import backend from './src/backend.js'
 import frontend from './src/frontend.js'
+import { env, exit } from 'process'
 
-backend.setRepository(await connect('es_test'))
+const database = env['DATABASE_NAME']
+if (!database) {
+  console.error('The environment variable DATABASE_NAME must be set')
+  exit(1)
+}
+
+backend.setRepository(await connect(database))
 backend.listenAtPort(8000)
 frontend.listenAtPort(80)
 console.log('Ready')
--- test/pg-task-repository.spec.ts
+++ test/pg-task-repository.spec.ts
@@ -2,15 +2,25 @@ import { assert } from 'chai'
 import { Task } from '../src/domain/task'
 import { connect } from '../src/pg-task-repository'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const database = process.env['TEST_DATABASE_NAME']!
+
 describe('PGTaskRepository', () => {
+  it('has environment', () => {
+    assert.ok(database, 'The environment variable TEST_DATABASE_NAME must be set')
+  })
+
   it('works', async () => {
-    const repo = await connect('es_test')
-    const task = new Task('Make PGTaskRepository work')
+    const repo = await connect(database)
 
-    await repo.add(task)
-    assert.ok(await repo.get(task.id))
-    assert.ok(await repo.getAll())
+    try {
+      const task = new Task('Make PGTaskRepository work')
 
+      await repo.add(task)
+      assert.ok(await repo.get(task.id))
+      assert.ok(await repo.getAll())
+    } finally {
       await repo.close()
+    }
   })
 })
