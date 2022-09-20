83b2565d66360c9376880ca566cb745faa26da3e
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-09-20 05:25:38 +0200

    Add Task.id

--- src/domain/task.ts
+++ src/domain/task.ts
@@ -1,8 +1,12 @@
+import { randomUUID } from 'crypto'
+
 export class Task {
+  readonly id: string
   isCompleted: boolean
   title: string
 
   constructor(title: string) {
+    this.id = randomUUID()
     this.isCompleted = false
     this.title = title
   }
--- test/domain/task.spec.ts
+++ test/domain/task.spec.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai'
+import { assert, expect } from 'chai'
 import { Task } from '../../src/domain/task'
 
 describe('Task', () => {
@@ -6,11 +6,26 @@ describe('Task', () => {
     const task = new Task('Get it done')
     expect(task.isCompleted).to.equal(false)
     expect(task.title).to.equal('Get it done')
+    assert.ok(task.id)
   })
 
   it('can be completed', () => {
     const task = new Task('Get it done')
     task.complete()
     expect(task.isCompleted).to.equal(true)
+  })
+
+  it('has a unique id', () => {
+    const task1 = new Task('Get it done')
+    const task2 = new Task('Get it done')
+
+    expect(task1.id).not.to.equal(task2.id)
+  })
+
+  it('has a uuid id', () => {
+    const task = new Task('Get it done')
+
+    expect(task.id).to.match(/^\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$/)
+    expect(task.id).to.match(/^[0-9a-f-]*$/)
   })
 })
