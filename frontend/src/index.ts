128348f6e29739dac5864ef1d7cde84a99c71ca1
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-10-01 16:19:39 +0200

    Animate completed tasks

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -33,18 +33,32 @@ async function updateTasks() {
 
   const taskListElement = document.getElementById('task-list')  
   if (taskListElement) {
-    const existingIds = [ ...taskListElement.getElementsByTagName('input') ]
-      .map((el: HTMLElement) => el.id)
-    const newTasks = tasks.filter((t: any) => existingIds.indexOf(t.id) < 0)
+    const newTasks = findNewTasks(taskListElement, tasks)
+    const oldElements = findObsoleteElements(taskListElement, tasks)
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    for (const element of oldElements) element.parentElement!.className = 'task hidden'
     for (const task of newTasks) task.isNew = true
+
+    await(delay(1000))
 
     taskListElement.innerHTML = await render('task-list', { tasks })
 
     await(delay(1))
-    console.log('new', [ ...taskListElement.getElementsByClassName('new') ])
-    for (const element of [ ...taskListElement.getElementsByClassName('new') ])
+    for (const element of [ ...taskListElement.getElementsByClassName('hidden') ])
       element.className = 'task'
   }
+}
+
+function findObsoleteElements(taskListElement: HTMLElement, tasks: any) {
+  return [ ...taskListElement.getElementsByTagName('input') ]
+    .filter(el => [ ...tasks ].every((t: any) => t.id !== el.id))
+}
+
+function findNewTasks(taskListElement: HTMLElement, tasks: any) {
+  const existingIds = [ ...taskListElement.getElementsByTagName('input') ]
+    .map((el: HTMLElement) => el.id)
+  return tasks.filter((t: any) => existingIds.indexOf(t.id) < 0)
 }
 
 function delay(millis: number) {
