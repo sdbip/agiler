99ec90d5ca74b8d27c03d2fcf03caf96953da3d0
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-10-25 06:01:06 +0200

    Extract some constants

--- backend/test/domain/item-add.spec.ts
+++ backend/test/domain/item-add.spec.ts
@@ -20,23 +20,35 @@ describe('Item.add', () => {
   })
 
   it('only updates parent once', () => {
-    const previousParent = reconstituteStoryWithChildren([ 'task_id' ], 'old_parent_id')
-    const newParent = reconstituteStory('new_parent_id')
-    const task = reconstituteTaskWithParent('old_parent_id', 'task_id')
-    previousParent.remove(task)
+    const taskId = 'task_id'
+    const oldParentId = 'old_parent_id'
+    const newParentId = 'new_parent_id'
+
+    const task = reconstituteTaskWithParent(oldParentId, taskId)
+    const oldParent = reconstituteStoryWithChildren([ taskId ], oldParentId)
+    oldParent.remove(task)
+
+    const newParent = reconstituteStory(newParentId)
     newParent.add(task)
+
     const events = task.unpublishedEvents.filter(e => e.name === 'ParentChanged')
     expect(events).to.have.lengthOf(1)
   })
 
   it('sets the last parent', () => {
-    const previousParent = reconstituteStoryWithChildren([ 'task_id' ], 'old_parent_id')
-    const newParent = reconstituteStory('new_parent_id')
-    const task = reconstituteTaskWithParent('old_parent_id', 'task_id')
-    previousParent.remove(task)
+    const taskId = 'task_id'
+    const oldParentId = 'old_parent_id'
+    const newParentId = 'new_parent_id'
+
+    const task = reconstituteTaskWithParent(oldParentId, taskId)
+    const oldParent = reconstituteStoryWithChildren([ taskId ], oldParentId)
+    oldParent.remove(task)
+
+    const newParent = reconstituteStory(newParentId)
     newParent.add(task)
+
     const event = task.unpublishedEvents.find(e => e.name === 'ParentChanged')
-    expect(event?.details.parent).to.equal(newParent.id)
+    expect(event?.details.parent).to.equal(newParentId)
   })
 
   it('throws if the Task already has a parent', () => {
@@ -48,16 +60,16 @@ describe('Item.add', () => {
   })
 
   it('throws if parent is not a Story', () => {
-    const task1 = reconstituteTask('id')
-    const task2 = reconstituteTask('id')
+    const task1 = reconstituteTask('task1_id')
+    const task2 = reconstituteTask('task2_id')
     expect(() => task1.add(task2)).to.throw()
     expect(task1.unpublishedEvents).to.have.lengthOf(0)
     expect(task2.unpublishedEvents).to.have.lengthOf(0)
   })
 
   it('throws if added item is not a Task', () => {
-    const task = reconstituteTask('id')
-    const story = reconstituteStory('id')
+    const task = reconstituteTask('task_id')
+    const story = reconstituteStory('story_id')
     expect(() => task.add(story)).to.throw()
     expect(task.unpublishedEvents).to.have.lengthOf(0)
     expect(story.unpublishedEvents).to.have.lengthOf(0)
--- backend/test/domain/item-remove.spec.ts
+++ backend/test/domain/item-remove.spec.ts
@@ -4,16 +4,22 @@ import { reconstituteStory, reconstituteStoryWithChildren, reconstituteTask, rec
 describe('Item.remove', () => {
 
   it('removes a Task from a Story', () => {
-    const story = reconstituteStoryWithChildren([ 'task_id' ], 'story_id')
-    const task = reconstituteTaskWithParent('story_id', 'task_id')
+    const taskId = 'task_id'
+    const storyId = 'story_id'
+
+    const story = reconstituteStoryWithChildren([ taskId ], storyId)
+    const task = reconstituteTaskWithParent(storyId, taskId)
     story.remove(task)
     const event = story.unpublishedEvents.find(e => e.name === 'ChildrenRemoved')
-    expect(event?.details.children).to.deep.equal([ task.id ])
+    expect(event?.details.children).to.deep.equal([ taskId ])
   })
 
   it('unsets the Task\'s parent', () => {
-    const story = reconstituteStoryWithChildren([ 'task_id' ], 'story_id')
-    const task = reconstituteTaskWithParent('story_id', 'task_id')
+    const taskId = 'task_id'
+    const storyId = 'story_id'
+
+    const story = reconstituteStoryWithChildren([ taskId ], storyId)
+    const task = reconstituteTaskWithParent(storyId, taskId)
     story.remove(task)
     const event = task.unpublishedEvents.find(e => e.name === 'ParentChanged')
     expect(event?.details.parent).to.equal(null)
