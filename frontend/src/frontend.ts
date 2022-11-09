17181ece54148c6fd6dab5dba581331e2f6d979c
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-11-09 05:49:03 +0100

    Replace index.html with a Mustache template

--- frontend/src/frontend.ts
+++ frontend/src/frontend.ts
@@ -2,17 +2,12 @@ import { promises as fs } from 'fs'
 import { setupServer } from '../../shared/src/server.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import Mustache from 'mustache'
 
 const server = setupServer({})
 
 server.public(resolve('../public'))
-server.get('/', async () => {
-  const data = await fs.readFile(resolve('../index.html'))
-  return {
-    statusCode: 200,
-    content: data.toString('utf-8'),
-  }
-})
+server.get('/', () => render('index'))
 
 server.get('/features', async () => {
   return {
@@ -28,6 +23,11 @@ export const stopListening = s.stopListening.bind(s)
 export default {
   listenAtPort,
   stopListening,
+}
+
+async function render(page: string) {
+  const data = await fs.readFile(resolve('../pages/template.mustache.html'))
+  return Mustache.render(data.toString('utf-8'), { page })
 }
 
 function resolve(relPath: string) {
