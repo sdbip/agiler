03b25a1dcc05be98da1d6b729b7337f21fdcd34a
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-12-04 11:51:23 +0100

    Set up for deployment

--- run.ts
+++ run.ts
@@ -1,7 +1,7 @@
 import readModel from './backend/src/read-model.js'
 import writeModel from './backend/src/write-model.js'
 import frontend from './frontend/src/frontend.js'
-import { env, exit } from 'process'
+import { env } from 'process'
 import { PGDatabase } from './backend/src/pg/pg-database.js'
 import { PGItemRepository } from './backend/src/pg/pg-item-repository.js'
 import { PGItemProjection } from './backend/src/pg/pg-item-projection.js'
@@ -10,24 +10,19 @@ import { PGEventRepository } from './backend/src/pg/pg-event-repository.js'
 import { promises as fs } from 'fs'
 import { PGRepository } from './backend/src/pg/pg-repository.js'
 
-const databaseName = env['DATABASE_NAME']
-if (!databaseName) {
-  console.error('The environment variable DATABASE_NAME must be set')
-  exit(1)
-}
-const database = await PGDatabase.connect(databaseName)
+const database = undefined as any as PGDatabase
 const repository = new PGRepository(database)
 
 const schemaDDL = await fs.readFile('./schema/schema.sql')
-await database.query(schemaDDL.toString('utf-8'))
+await database?.query(schemaDDL.toString('utf-8'))
 
 readModel.setRepository(new PGItemRepository(database))
-readModel.listenAtPort(Number(env['READ_MODEL_PORT']))
+readModel.listenAtPort(Number(env['READ_MODEL_PORT'] ?? 9999))
 
 writeModel.setEventProjection(new PGItemProjection(database))
 writeModel.setEventRepository(new PGEventRepository(repository))
 writeModel.setPublisher(new PGEventPublisher(repository))
-writeModel.listenAtPort(Number(env['WRITE_MODEL_PORT']))
+writeModel.listenAtPort(Number(env['WRITE_MODEL_PORT'] ?? 8888))
 
 frontend.listenAtPort(80)
 
